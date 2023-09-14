892d8f2dd73fdd563d2d29913a3dcb0ef0fb201a
Huy <laiquanghuy24122001@gmail.com>
2023-09-14 14:36:32 +0700

    .

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import io
+from shutil import copyfileobj
 
+import cv2
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import FileResponse
 from PIL import Image
@@ -7,12 +9,25 @@ from PIL import Image
 app = FastAPI()
 
 
-@app.post('/grayscale')
-async def grayscale(file: UploadFile):
-    if file.filename.split('.')[-1].lower() in {'jpg', 'jpeg', 'png'}:
-        out = 'grayscale.png'
-        Image.open(io.BytesIO(await file.read())).convert('L').save(out)
-        return FileResponse(
-            out,
-            headers={'Content-Disposition': f'attachment; filename={out}'},
-        )
+def validate_image(file: UploadFile):
+    if file.filename.split('.')[-1].lower() not in {'jpg', 'jpeg', 'png'}:
+        raise ValueError('File must be an image')
+
+
+@app.post('/grayscale_PIL')
+async def grayscale_PIL(file: UploadFile):
+    validate_image(file)
+    out = 'grayscale.png'
+    Image.open(io.BytesIO(await file.read())).convert('L').save(out)
+    return FileResponse(out)
+
+
+@app.post('/grayscale_cv2')
+async def grayscale_cv2(file: UploadFile):
+    validate_image(file)
+    fn = file.filename
+    with open(fn, 'wb') as f:
+        copyfileobj(file.file, f)
+    out = 'grayscale.png'
+    cv2.imwrite(out, cv2.cvtColor(cv2.imread(fn), cv2.COLOR_BGR2GRAY))
+    return FileResponse(out)
